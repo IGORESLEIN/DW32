b97a479533d00b74e0113de4ef25464e1ce89a95
Igor <igoreslein@gmail.com>
2016-10-18 08:32:16 +0000

    phpmailer

--- SERVIDOR/COMPOSER/ejemplo.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "vendor/autoload.php";
-$variable = $probatzeko = array('uno' => 1, 'segundo' => 'froga', 'hirukoa' => true);
-d($variable);
-?>
---
+++ SERVIDOR/COMPOSER/phpmailer.php
@@ -0,0 +1,30 @@
+<?php
+require "vendor/autoload.php";
+$mail = new PHPMailer;
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'iuriaur15dw@ikzubirimanteo.com';                 // SMTP username
+$mail->Password = '943734394';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+$mail->setFrom('iuriaur15dw@ikzubirimanteo.com', 'PHPMailer');
+$mail->addAddress('igoreslein@gmail.com', 'IGORESLEIN');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+$mail->Subject = 'Frogatzen';
+$mail->Body    = 'Kaixoooooo!!!!!!!!!!';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
