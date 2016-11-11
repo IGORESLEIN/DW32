2f993759d11c7a6c891a9f115caf1e63125dcd2f
Igor <igoreslein@gmail.com>
2016-11-11 12:04:06 +0000

    7'5

--- SERVIDOR/api/index.php
+++ SERVIDOR/api/index.php
@@ -1,83 +1,104 @@
 <?php
-if (!$_POST){
-   //si no recibo nada por post, muestro una página con un formulario de carga de la imagen
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+// Load the Google API PHP Client Library.
+require_once __DIR__ . '/vendor/autoload.php';
 
-<html>
-<head>
-   <title>enviando archivo</title>
-</head>
+// Start a session to persist credentials.
+session_start();
 
-<body>
+// Create the client object and set the authorization configuration
+// from the client_secretes.json you downloaded from the developer console.
+$client = new Google_Client();
+$client->setAuthConfig(__DIR__ . '/client_secrets.json');
+$client->addScope(Google_Service_Analytics::ANALYTICS_READONLY);
 
-<form action="cargar-imagen-pixrl.php" method="post" enctype="multipart/form-data"> 
-<b>Enviar un nuevo archivo: </b> 
-<br> 
-<input name="image" type="file"> 
-<br> 
-<input type="submit" name="submitir" value="Enviar"> 
-</form> 
+// If the user has already authorized this app then get an access token
+// else redirect to ask the user to authorize access to Google Analytics.
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+  // Set the access token on the client.
+  $client->setAccessToken($_SESSION['access_token']);
+
+  // Create an authorized analytics service object.
+  $analytics = new Google_Service_Analytics($client);
+
+  // Get the first view (profile) id for the authorized user.
+  $profile = getFirstProfileId($analytics);
+
+  // Get the results from the Core Reporting API and print the results.
+  $results = getResults($analytics, $profile);
+  printResults($results);
+} else {
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/SERVIDOR/api/oauth2callback.php';
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+}
 
 
-</body>
-</html>
-<?php
-}else{
+function getFirstProfileId($analytics) {
+  // Get the user's first view (profile) ID.
 
-   //Ver si recibo imagen en $_FILES
-   if(empty($_FILES['image'])) {
-      echo "No he recibido la imagen";
-   exit;
-   }
-   //ver si es un upload error
-   if($_FILES['image']['error'] != UPLOAD_ERR_OK) {
-      echo "Error escribiendo el archivo al disco ". $_FILES['image']['error'];
-   }
-   
-   //Recibo parámetros del archivo de upload
-   $archivo = $_FILES['image']['tmp_name'];
-   $tipo = $_FILES['image']['type'];
-   $nombre_original = $_FILES['image']['name'];
-   $tamano = $_FILES['userfile']['size'];
-   
-   //calculo la ruta donde voy a guardar el fichero
-   //quizás te interesaría editar el $nombre_original para que sea único en tu servidor y no machacar otras fotos y que el nombre del archivo tampoco tenga caracteres raros que puedan molestar.
-   $ruta_guardar = "images/". $nombre_original;
-   
-   //aquí podría validar el fichero (esto son sólo un par de validaciones de prueba)
-   if (strpos($tipo_archivo, "jpg") === 0){
-      echo "Sólo se permiten archivos jpg";
-      exit;
-   }
-   if ($tamano > (1024 * 1024)){
-      echo "Sólo se permiten archivos de 1 mega máximo";
-   }
-   
-   
-   //Copio el fichero en el servidor
-   if(!move_uploaded_file($archivo, $ruta_guardar)) {
-      echo "Error copiando el fichero";
-   exit;
-   }
-   
-   //redirijo al navegador al servicio de edición de la foto en Pixlr
-   //idioma de la interfaz de pixlr
-   $parametros = "loc=es";
-   //imagen que queremos poner para editar
-   $parametros .= "I=" . urlencode("http://www.desarrolloweb.com/probando/" . $ruta_guardar);
-   //título de la imagen
-   $parametros .= "&title=" . urlencode("foto");
-   //página a la que redirigir en caso que se salgan de pixlr
-   $parametros .= "&exit=" . urlencode("http://www.desarrolloweb.com");
-   //method por el que me enviarán los datos de la imagen editada
-   $parametros .= "&method=GET";
-   //nombre de mi sitio web, que mostrar a la hora de guardar como
-   $parametros .= "&referrer=en%20DesarrolloWeb.com";
-   //página a la que se enviarán los datos de la imagen editada
-   $parametros .= "&target=" . urlencode("http://www.desarrolloweb.com/probando/salvar-imagen-pixrl.php");
-   
-   header("Location: http://www.pixlr.com/editor/?" . $parametros);
-   
-}   
+  // Get the list of accounts for the authorized user.
+  $accounts = $analytics->management_accounts->listManagementAccounts();
+
+  if (count($accounts->getItems()) > 0) {
+    $items = $accounts->getItems();
+    $firstAccountId = $items[0]->getId();
+
+    // Get the list of properties for the authorized user.
+    $properties = $analytics->management_webproperties
+        ->listManagementWebproperties($firstAccountId);
+
+    if (count($properties->getItems()) > 0) {
+      $items = $properties->getItems();
+      $firstPropertyId = $items[0]->getId();
+
+      // Get the list of views (profiles) for the authorized user.
+      $profiles = $analytics->management_profiles
+          ->listManagementProfiles($firstAccountId, $firstPropertyId);
+
+      if (count($profiles->getItems()) > 0) {
+        $items = $profiles->getItems();
+
+        // Return the first view (profile) ID.
+        return $items[0]->getId();
+
+      } else {
+        throw new Exception('No views (profiles) found for this user.');
+      }
+    } else {
+      throw new Exception('No properties found for this user.');
+    }
+  } else {
+    throw new Exception('No accounts found for this user.');
+  }
+}
+
+function getResults($analytics, $profileId) {
+  // Calls the Core Reporting API and queries for the number of sessions
+  // for the last seven days.
+  return $analytics->data_ga->get(
+      'ga:' . $profileId,
+      '7daysAgo',
+      'today',
+      'ga:sessions');
+}
+
+function printResults($results) {
+  // Parses the response from the Core Reporting API and prints
+  // the profile name and total sessions.
+  if (count($results->getRows()) > 0) {
+
+    // Get the profile name.
+    $profileName = $results->getProfileInfo()->getProfileName();
+
+    // Get the entry for the first entry in the first row.
+    $rows = $results->getRows();
+    $sessions = $rows[0][0];
+
+    // Print the results.
+    print "<p>First view (profile) found: $profileName</p>";
+    print "<p>Total sessions: $sessions</p>";
+  } else {
+    print "<p>No results found.</p>";
+  }
+}
+
 ?>
